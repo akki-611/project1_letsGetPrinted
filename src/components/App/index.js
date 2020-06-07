10575033943c4ad1695babb315ee975b4a6dc64e
Ankit Sharma <Ankit.Sharma.3@team.telstra.com>
2020-06-07 14:12:00 +0530

    first commit

---
+++ src/components/App/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Shelf from '../Shelf';
+import Filter from '../Shelf/Filter';
+// import GithubCorner from '../github/Corner';
+import FloatCart from '../FloatCart';
+import StarButton from '../Title/HeaderFile';
+
+const App = () => (
+  <React.Fragment>
+    {/* <GithubCorner /> */}
+    <StarButton />
+    <main>
+      <Filter />
+      <Shelf />
+    </main>
+    <FloatCart />
+  </React.Fragment>
+);
+
+export default App;
---
+++ src/components/Title/HeaderFile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './style.css';
+
+const StarButton = () => (
+    <div>
+    <span>"logo"</span>
+  <span className="star-button-container">
+       <strong>Get Printed</strong>
+  </span>
+  </div>
+);
+
+export default StarButton;
